dbbfc6cf14443801aa074795a94f8af42dba0170
tightwap20 <tightwap20@gmail.com>
2013-09-18 10:51:05 -0700

    adding files from to do list website

---
+++ todolist.js
@@ -0,0 +1,38 @@
+function addItem() {
+	
+	var texBox = document.getElementById('listInput');
+	var val = texBox.value;
+	var ele = texBox.id;
+	texBox.value = "";
+	var newItem = document.createElement("li"); //creating element "li" in html
+	newItem.id = "listVal";
+	var textNode = document.createTextNode(val); //creating a new variable that is taking the textbox value
+    newItem.appendChild(textNode); //appending new item or the li element to the textNode variable
+    document.getElementById('listItems').appendChild(newItem); //appending newItem variable to listItems ol
+	var deleteBox = document.createElement('input');
+	deleteBox.type = "checkbox";
+	deleteBox.name = "item"
+	newItem.appendChild(deleteBox); //appending deleteBox to newItem variable
+	
+};
+
+function deleteItem() {
+	var textItem = document.getElementsByName('item'); //grabbing the checkbox, name "item"
+	for (i=0; i<textItem.length; i++) //loop through the checkbox
+		{
+		if(textItem[i].checked) //if it is checked
+			{
+			var remItem = textItem[i]; // creating a variable to reference textItem element to be removed
+			var liItem = document.getElementById('listItems'); //
+			remItem.parentNode.removeChild(remItem); //grabbing the parent of item element, which is the "li", and removing the child, which is the text box
+			ol = document.getElementsByTagName('ol')[0]; //setting ol = to the html element tag 'ol' in the zero position??
+			li = ol.getElementsByTagName('li'); //setting li = to the ol.li
+			ol.removeChild(li[i]); //removing the ol child li[i] through the loop??
+		}
+		else{
+
+		}
+	}
+
+}
+
